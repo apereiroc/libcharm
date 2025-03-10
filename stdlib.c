e56fc9bcc91d22edd6ac7ba1c93780547ca33402
Asier <asier.pereiro.castro@cern.ch>
2025-03-10 22:16:51 +0100

    add compiler stuff to exit

--- stdlib.c
+++ stdlib.c
@@ -1,4 +1,7 @@
 #include "stdlib.h"
 #include "syscall.h"
 
-void exit(int exit_code) { syscall1(SYS_exit, exit_code); }
+__attribute__((noreturn)) void exit(int exit_code) {
+  syscall1(SYS_exit, exit_code);
+  __builtin_unreachable();
+}
