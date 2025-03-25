bc3346c05b8b8a866f3f8ee72b65f1ebfca1d450
Asier <asier.pereiro.castro@cern.ch>
2025-03-25 18:11:32 +0100

    test, build: add testing framework

---
+++ tests/testing_lib.c
@@ -0,0 +1,42 @@
+#include "syscall.h"
+
+// it is assumed that all of the functions used here work perfectly :-)
+
+// basic write
+long __write(int fd, const void *buf, long count) {
+  return syscall3(SYS_write, fd, (long)buf, count);
+}
+
+// basic exit
+void __exit(int status) {
+  syscall1(SYS_exit, status);
+  __builtin_unreachable();
+}
+
+// basic strlen
+long __str_len(const char *str) {
+  long len = 0;
+  while (*str != '\0') {
+    len++;
+    str++;
+  }
+
+  return len;
+}
+
+void print_success(void) { __write(1, "All tests passed\n", 17); }
+
+void assert_true(int condition, const char *msg) {
+  if (!condition) {
+    long msg_len = __str_len(msg);
+
+    __write(2, "Test failed: ", 13);
+    __write(2, msg, msg_len);
+    __write(2, "\n", 1);
+    __exit(1);
+  }
+}
+
+void assert_false(int condition, const char *msg) {
+  assert_true(!condition, msg);
+}
---
+++ tests/testing_lib.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void print_success(void);
+void assert_true(int condition, const char *msg);
+void assert_false(int condition, const char *msg);
