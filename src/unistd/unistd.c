41cfa8c8538d6f79cf47cde0c206e24aec503e17
Asier Pereiro <asier.pereiro.castro@cern.ch>
2025-03-26 13:04:52 +0100

    remove global c files

--- src/unistd/unistd.c
+++
@@ -1,8 +0,0 @@
-#include "unistd.h"
-#include "stddef.h"
-#include "syscall.h"
-
-ssize_t write(int fildes, const void *buf, size_t nbyte) {
-  ssize_t res = syscall3(SYS_write, fildes, (long)buf, (long)nbyte);
-  return res;
-}
