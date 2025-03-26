12757e83120f30ddf7a5bda33a00e43a2bc2f705
Asier Pereiro <asier.pereiro.castro@cern.ch>
2025-03-26 11:20:23 +0100

    test: provide test for strlen

---
+++ tests/test_string.c
@@ -0,0 +1,15 @@
+#include "string.h"
+#include "testing_lib.h"
+
+static inline void test_strlen(void) {
+  assert_true(strlen("123") == 2, "strlen test failed");
+  assert_true(strlen("123 456 789") == 11, "strlen test failed");
+  assert_true(strlen("") == 0, "strlen empty string test failed");
+}
+
+int main(void) {
+  test_strlen();
+
+  print_success();
+  return 0;
+}
