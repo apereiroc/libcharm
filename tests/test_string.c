d5ffddf51194ac50437b216697da4ef61536f389
Asier Pereiro <asier.pereiro.castro@cern.ch>
2025-03-26 11:34:17 +0100

    test: fix not-working test when trying to get an error :-)

--- tests/test_string.c
+++ tests/test_string.c
@@ -2,7 +2,7 @@
 #include "testing_lib.h"
 
 static inline void test_strlen(void) {
-  assert_true(strlen("123") == 2, "strlen test failed");
+  assert_true(strlen("123") == 3, "strlen test failed");
   assert_true(strlen("123 456 789") == 11, "strlen test failed");
   assert_true(strlen("") == 0, "strlen empty string test failed");
 }
