8278ab41314452cdb93bacb5609b29d50d3cf12b
Asier <asier.pereiro.castro@cern.ch>
2025-04-04 19:44:28 +0200

    test(string): add test for memcpy

--- tests/test_string.c
+++ tests/test_string.c
@@ -1,7 +1,7 @@
 #include "string.h"
 #include "testing_lib.h"
 
-static inline void test_strlen1(void) {
+static inline void test_strlen(void) {
   assert_eq(strlen(""), 0, "empty string");
   assert_eq(strlen("A"), 1, "single char");
   assert_eq(strlen("hello"), 5, "short word");
@@ -9,8 +9,21 @@ static inline void test_strlen1(void) {
   assert_eq(strlen("|V|y `c@de` !$ wºrk!ng"), 23, "special chars");
 }
 
+static inline void test_memcpy(void) {
+  const char src[] = "hello!";
+  char dest[6];
+  memcpy(dest, src, 6);
+  assert_eq(dest[0], 'h', "testing memcpy - char `h`");
+  assert_eq(dest[1], 'e', "testing memcpy - char `e`");
+  assert_eq(dest[2], 'l', "testing memcpy - char `l`");
+  assert_eq(dest[3], 'l', "testing memcpy - char `l`");
+  assert_eq(dest[4], 'o', "testing memcpy - char `o`");
+  assert_eq(dest[5], '!', "testing memcpy - char `!`");
+}
+
 int main(void) {
-  TEST_FUNCTION(test_strlen1);
+  TEST_FUNCTION(test_strlen);
+  TEST_FUNCTION(test_memcpy);
 
   return 0;
 }
