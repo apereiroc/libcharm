b7301cda095db9a13f761e61ee922b98e26e784e
Asier Pereiro <asier.pereiro.castro@cern.ch>
2025-04-04 18:36:30 +0200

    fix: ret not working. reuse register x0

--- src/syscall/syscall.c
+++ src/syscall/syscall.c
@@ -136,7 +136,7 @@ inline long syscall6(long number, long arg1, long arg2, long arg3, long arg4,
  *   - syscall number goes to x8 (Linux) / x16 (Darwin)
  *   - arguments are passed to: x0, x1, x2, x3, x4, x5
  *   - the system call is performed by calling svc 0
- *   - syscall return comes in x0
+ *   - syscall return comes in x0.
  *   - rcx and r11 are clobbered, others are preserved.
  *   - the arguments are cast to long and assigned into the target registers
  *     which are then simply passed as registers to the asm code, so that we
@@ -144,137 +144,139 @@ inline long syscall6(long number, long arg1, long arg2, long arg3, long arg4,
  */
 
 inline long syscall0(long number) {
-  long ret;
 #if defined(__APPLE__)
-  register long _number __asm__("x16") = (number);
+  register long _num __asm__("x16") = number;
 #elif defined(__linux__)
-  register long _number __asm__("x8") = (number);
+  register long _num __asm__("x8") = number;
 #endif
-  __asm__ volatile("svc 0\n" : "=r"(ret) : "r"(_number) : "memory", "cc");
+  register long x0 __asm__("x0");
 
-  return ret;
+  __asm__ volatile("svc 0\n" : "=r"(x0) : "r"(_num) : "memory", "cc");
+
+  // syscall output is in x0
+  return x0;
 }
 
 inline long syscall1(long number, long arg1) {
 
-  long ret;
 #if defined(__APPLE__)
-  register long _number __asm__("x16") = (number);
+  register long _num __asm__("x16") = number;
 #elif defined(__linux__)
-  register long _number __asm__("x8") = (number);
+  register long _num __asm__("x8") = number;
 #endif
-  register long _arg1 __asm__("x0") = (long)(arg1);
-  __asm__ volatile("svc 0\n"
-                   : "=r"(ret)
-                   : "r"(_arg1), "r"(_number)
-                   : "memory", "cc");
+  register long x0 __asm__("x0") = arg1;
 
-  return ret;
+  __asm__ volatile("svc 0\n" : "=r"(x0) : "r"(x0), "r"(_num) : "memory", "cc");
+
+  // syscall output is in x0
+  return x0;
 }
 
 inline long syscall2(long number, long arg1, long arg2) {
 
-  long ret;
 #if defined(__APPLE__)
-  register long _number __asm__("x16") = (number);
+  register long _num __asm__("x16") = number;
 #elif defined(__linux__)
-  register long _number __asm__("x8") = (number);
+  register long _num __asm__("x8") = number;
 #endif
-  register long _arg1 __asm__("x0") = (long)(arg1);
-  register long _arg2 __asm__("x1") = (long)(arg2);
+  register long x0 __asm__("x0") = arg1;
+  register long x1 __asm__("x1") = arg2;
+
   __asm__ volatile("svc 0\n"
-                   : "=r"(ret)
-                   : "r"(_arg1), "r"(_arg2), "r"(_number)
+                   : "=r"(x0)
+                   : "r"(x0), "r"(x1), "r"(_num)
                    : "memory", "cc");
 
-  return ret;
+  // syscall output is in x0
+  return x0;
 }
 
 inline long syscall3(long number, long arg1, long arg2, long arg3) {
 
-  long ret;
 #if defined(__APPLE__)
-  register long _number __asm__("x16") = (number);
+  register long _num __asm__("x16") = number;
 #elif defined(__linux__)
-  register long _number __asm__("x8") = (number);
+  register long _num __asm__("x8") = number;
 #endif
-  register long _arg1 __asm__("x0") = (long)(arg1);
-  register long _arg2 __asm__("x1") = (long)(arg2);
-  register long _arg3 __asm__("x2") = (long)(arg3);
+  register long x0 __asm__("x0") = arg1;
+  register long x1 __asm__("x1") = arg2;
+  register long x2 __asm__("x2") = arg3;
+
   __asm__ volatile("svc 0\n"
-                   : "=r"(ret)
-                   : "r"(_arg1), "r"(_arg2), "r"(_arg3), "r"(_number)
+                   : "=r"(x0)
+                   : "r"(x0), "r"(x1), "r"(x2), "r"(_num)
                    : "memory", "cc");
 
-  return ret;
+  // syscall output is in x0
+  return x0;
 }
 
 inline long syscall4(long number, long arg1, long arg2, long arg3, long arg4) {
 
-  long ret;
 #if defined(__APPLE__)
-  register long _number __asm__("x16") = (number);
+  register long _num __asm__("x16") = number;
 #elif defined(__linux__)
-  register long _number __asm__("x8") = (number);
+  register long _num __asm__("x8") = number;
 #endif
-  register long _arg1 __asm__("x0") = (long)(arg1);
-  register long _arg2 __asm__("x1") = (long)(arg2);
-  register long _arg3 __asm__("x2") = (long)(arg3);
-  register long _arg4 __asm__("x3") = (long)(arg4);
+  register long x0 __asm__("x0") = arg1;
+  register long x1 __asm__("x1") = arg2;
+  register long x2 __asm__("x2") = arg3;
+  register long x3 __asm__("x3") = arg4;
+
   __asm__ volatile("svc 0\n"
-                   : "=r"(ret)
-                   : "r"(_arg1), "r"(_arg2), "r"(_arg3), "r"(_arg4),
-                     "r"(_number)
+                   : "=r"(x0)
+                   : "r"(x0), "r"(x1), "r"(x2), "r"(x3), "r"(_num)
                    : "memory", "cc");
 
-  return ret;
+  // syscall output is in x0
+  return x0;
 }
 
 inline long syscall5(long number, long arg1, long arg2, long arg3, long arg4,
                      long arg5) {
-
-  long ret;
 #if defined(__APPLE__)
-  register long _number __asm__("x16") = (number);
+  register long _num __asm__("x16") = number;
 #elif defined(__linux__)
-  register long _number __asm__("x8") = (number);
+  register long _num __asm__("x8") = number;
 #endif
-  register long _arg1 __asm__("x0") = (long)(arg1);
-  register long _arg2 __asm__("x1") = (long)(arg2);
-  register long _arg3 __asm__("x2") = (long)(arg3);
-  register long _arg4 __asm__("x3") = (long)(arg4);
-  register long _arg5 __asm__("x4") = (long)(arg5);
+  register long x0 __asm__("x0") = arg1;
+  register long x1 __asm__("x1") = arg2;
+  register long x2 __asm__("x2") = arg3;
+  register long x3 __asm__("x3") = arg4;
+  register long x4 __asm__("x4") = arg5;
+
   __asm__ volatile("svc 0\n"
-                   : "=r"(ret)
-                   : "r"(_arg1), "r"(_arg2), "r"(_arg3), "r"(_arg4), "r"(_arg5),
-                     "r"(_number)
+                   : "=r"(x0)
+                   : "r"(x0), "r"(x1), "r"(x2), "r"(x3), "r"(x4), "r"(_num)
                    : "memory", "cc");
 
-  return ret;
+  // syscall output is in x0
+  return x0;
 }
 
 inline long syscall6(long number, long arg1, long arg2, long arg3, long arg4,
                      long arg5, long arg6) {
 
-  long ret;
 #if defined(__APPLE__)
-  register long _number __asm__("x16") = (number);
+  register long _num __asm__("x16") = number;
 #elif defined(__linux__)
-  register long _number __asm__("x8") = (number);
+  register long _num __asm__("x8") = number;
 #endif
-  register long _arg1 __asm__("x0") = (long)(arg1);
-  register long _arg2 __asm__("x1") = (long)(arg2);
-  register long _arg3 __asm__("x2") = (long)(arg3);
-  register long _arg4 __asm__("x3") = (long)(arg4);
-  register long _arg5 __asm__("x4") = (long)(arg5);
-  register long _arg6 __asm__("x5") = (long)(arg6);
+  register long x0 __asm__("x0") = arg1;
+  register long x1 __asm__("x1") = arg2;
+  register long x2 __asm__("x2") = arg3;
+  register long x3 __asm__("x3") = arg4;
+  register long x4 __asm__("x4") = arg5;
+  register long x5 __asm__("x5") = arg6;
+
   __asm__ volatile("svc 0\n"
-                   : "=r"(ret)
-                   : "r"(_arg1), "r"(_arg2), "r"(_arg3), "r"(_arg4), "r"(_arg5),
-                     "r"(_arg6), "r"(_number)
+                   : "=r"(x0)
+                   : "r"(x0), "r"(x1), "r"(x2), "r"(x3), "r"(x4), "r"(x5),
+                     "r"(_num)
                    : "memory", "cc");
 
-  return ret;
+  // syscall output is in x0
+  return x0;
 }
 
 #endif
